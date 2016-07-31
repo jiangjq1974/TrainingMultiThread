84a3f2c8a9d7eb5602067c3d0495e74a7c8c1751
jjq <jjq>
2016-07-31 17:02:48 +0800

    AsyncTask

--- app/src/main/java/com/example/lsx/trainingmulitiplethread/MainActivity.java
+++ app/src/main/java/com/example/lsx/trainingmulitiplethread/MainActivity.java
@@ -2,17 +2,23 @@ package com.example.lsx.trainingmulitiplethread;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     private ImageView mImageview;
     private Button mLoadimagebutton;
     private Button mToastbutton;
+    private ProgressBar mProgressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         mImageview = (ImageView) findViewById(R.id.Main_Activity_Image_View);
         mLoadimagebutton = (Button) findViewById(R.id.Main_Activity_Load_Image_Button);
         mToastbutton = (Button) findViewById(R.id.Main_Activity_Toast_Button);
+        mProgressbar = (ProgressBar) findViewById(R.id.activity_main_progress_bar);
         mToastbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         mLoadimagebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showimage();
+                new LoadImageTask().execute();
             }
         });
     }
@@ -40,11 +47,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
-                try {
-                    Thread.sleep(4000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                sleep();
                 mImageview.post(new Runnable() {
                     @Override
                     public void run() {
@@ -53,5 +56,47 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }).start();
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class LoadImageTask extends AsyncTask<Void, Integer, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+            for (int i = 0; i <11 ; i++) {
+                sleep();
+                publishProgress(i*10);
+            }
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
+            Log.d(TAG, "Thread: "+Thread.currentThread().getName());
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            mImageview.setImageBitmap(bitmap);
+            Log.d(TAG, "Thread: "+Thread.currentThread().getName());
+            mProgressbar.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            mProgressbar.setProgress(values[0]);
+        }
     }
 }
